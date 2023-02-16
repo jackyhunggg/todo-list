db7a8e75ccdbe866652b063099ffe3d37f470443
Jacky Hung <jacky85725@gmail.com>
2023-02-16 12:09:09 +0800

    connect to db and add mongoose

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+// 第一行先把 mongoose 載入進來，才能使用相關方法
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+// 把想要的資料結構當成參數傳給 new Schema()
+const todoSchema = new Schema ({
+    name: {
+        type: string,
+        required: true,
+    },
+    done: {
+        type:bolean,
+    },
+})
+// mongoose.model 會複製我們定義的 Schema 並編譯成一個可供操作的 model 物件
+// 匯出的時候我們把這份 model 命名為 Todo，以後在其他的檔案直接使用 Todo 就可以操作和「待辦事項」有關的資料了
+module.exports = mongoose.model('Todo', todoSchema)
