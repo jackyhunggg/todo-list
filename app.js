6a076f04a1a45e2df54ffdb52240059f48d226c1
Jacky Hung <jacky85725@gmail.com>
2023-03-02 22:02:01 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
-// 取得資料庫連線狀態
-const db = mongoose.connection
 const bodyParser = require('body-parser')
 // 載入 Todo model
 const Todo = require('./models/todo')
 // 載入 method-override
 const methodOverride = require('method-override')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 // 引用路由器
 const routes = require('./routes')
+// 對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -22,21 +27,6 @@ app.use(methodOverride('_method'))
 // 將 request 導入路由器
 app.use(routes)
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.listen(port, () => {
     console.log(`I'm listening on localhost:${port}`)
