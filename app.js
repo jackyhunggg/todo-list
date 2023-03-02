55fa344d457d7c8a0c4b2e27a0b019ad910b1495
Jacky Hung <jacky85725@gmail.com>
2023-03-02 22:07:09 +0800

    small fix

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
@@ -8,10 +12,7 @@ const Todo = require('./models/todo')
 // 載入 method-override
 const methodOverride = require('method-override')
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+
 
 // 引用路由器
 const routes = require('./routes')
