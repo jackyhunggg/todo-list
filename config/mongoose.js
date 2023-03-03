de756e02fea63f8c60de45334a16c7a117d4fff9
Jacky Hung <jacky85725@gmail.com>
2023-03-03 23:43:32 +0800

    minor fix

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
