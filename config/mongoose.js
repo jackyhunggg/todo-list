931e0f253c2c6ec749ab9173159e32255bf6a6a2
Jacky Hung <jacky85725@gmail.com>
2023-03-04 01:04:08 +0800

    ???????

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
